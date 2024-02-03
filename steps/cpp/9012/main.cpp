5f4a9562433a4b4d6f50b5d6b8c1d79e9d472419
sangjw2 <sangjw2@gmail.com>
2024-02-03 20:19:10 +0900

    9012번 메모리초과

---
+++ steps/cpp/9012/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int size;
+    int index;
+    int* ptr;
+public:
+    Stack(int s) : size(s) {
+        if(ptr!=nullptr) delete[] ptr; // 기존에 있으면 삭제
+        else {
+            ptr = new int[size];
+            index = 0;
+        }
+    }
+    ~Stack() {
+        if(ptr!=nullptr) delete[] ptr;
+    }
+
+    void push(int a){
+        if(index<size) {
+            *(ptr+(index++)) = a;
+        } else cout << "Stack is Full.\n";
+    }
+
+    bool pop() {
+        if(index>0) {
+            *(ptr+(index--)) = 0;
+            // if(!index) return false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    friend bool checkVPS(char*);
+};
+
+int getLength(char* n) {
+    int length{0};
+    while(*(n+length)!='\0') {
+        length++;
+    }
+    return length;
+}
+
+bool checkVPS(char* s) {
+    int* cnt = new int{0};
+    int* len = new int;
+    *len = getLength(s);
+    Stack* ps = new Stack(*len);
+    for(int i=0;i<*len;i++) {
+        if(*(s+i)=='(') {
+            ps->push(1);
+        } else { // ) 일 경우
+            if(!(ps->pop())) {
+                delete ps;
+                delete len;
+                delete cnt;
+                return false;
+            }
+        }
+    }
+    if((ps->index)>0) {
+        delete ps;
+        delete len;
+        delete cnt;
+        return false;
+    }
+    delete ps;
+    delete len;
+    delete cnt;
+    return true;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int t;
+    char** strings;
+
+    cin >> t;
+    strings = new char*[t];
+
+    for(int i=0;i<t;i++) {
+        strings[i] = new char[51];
+    }
+
+
+
+    for(int i=0;i<t;i++) {
+        cin >> strings[i];
+    }
+    for(int i=0;i<t;i++) {
+        if(checkVPS(strings[i])) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+
+
+
+    for(int i=0;i<t;i++){
+        delete[] strings[i];
+    }
+    delete[] strings;
+
+    return 0;
+}
